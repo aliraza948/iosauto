59b33134d66dbf2e768504f630c93aae22ea8759
aliraza948 <aliraza948533@gmail.com>
2025-03-05 12:04:17 +0500

    good

--- ios/index.ts
+++ ios/index.ts
@@ -7,11 +7,11 @@ import paparse from 'papaparse'
 import readline from 'readline'
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
-let data:{
-    seq:string,
-    number:string,
-    name:string
-}[]=[]
+let data: {
+    seq: string,
+    number: string,
+    name: string
+}[] = []
 
 
 
@@ -26,16 +26,16 @@ const logger = winston.createLogger({
     ),
     transports: [
         //  new winston.transports.Console(), // Logs to the console
-        new winston.transports.File({ filename: path.join(__dirname+'/logs/app.log') }) // Logs to a file
+        new winston.transports.File({ filename: path.join(__dirname + '/logs/app.log') }) // Logs to a file
     ],
 });
 import axios from "axios"
 const capabilities = {
     "platformName": "iOS",
-    "appium:wdaBaseUrl":"http://10.0.0.27",
-    "appium:wdaLocalPort":8100,
-    "appium:automationName":"XCUITest",
-    "appium:udid":"00008020-0015744C2608003A"
+    "appium:wdaBaseUrl": "http://10.0.0.27",
+    "appium:wdaLocalPort": 8100,
+    "appium:automationName": "XCUITest",
+    "appium:udid": "00008020-0015744C2608003A"
 };
 const wdOpts = {
     hostname: 'localhost',
@@ -43,69 +43,74 @@ const wdOpts = {
     logLevel: "error",
     capabilities,
     path: '/wd/hub',
-} 
-const RandomMax=(max:number):number=>{
-    return Math.ceil(Math.random()*max)
 }
-async function sleep(ms:number) {
-    return new Promise(r=>setTimeout(r,ms))
+const RandomMax = (max: number): number => {
+    return Math.ceil(Math.random() * max)
+}
+async function sleep(ms: number) {
+    return new Promise(r => setTimeout(r, ms))
 }
 async function runTest() {
     const driver = await remote(wdOpts) as Browser
     process.stdin.on("keypress", (str, key) => {
-  if (key.name === "j") {
-    console.log("\n'Q' key pressed. Exiting...");
-    const csv=paparse.unparse(data)
-    fs.writeFileSync("./data.csv", csv, "utf8");
-    process.exit(0)
-    
-  }
-});
-    
+        if (key.name === "j") {
+            console.log("\n'Q' key pressed. Exiting...");
+            const csv = paparse.unparse(data)
+            fs.writeFileSync("./data.csv", csv, "utf8");
+            process.exit(0)
+
+        }
+    });
+
     try {
-       let ids=new Set()
-       logger.info(`Starting`)
-       let tr=1
-       let ny
-       while (true){
-        
-      const elements=await  driver.$('//XCUIElementTypeTextField[@name="Name, $Cashtag, Phone, Email"]')
-      
-      const text=await elements.getText()
-      if(text){
-        ny=Number(text)
-        const el=await driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeButton)[1]')
-      const pic=await driver.takeElementScreenshot(el.elementId)
-      const name=await driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeStaticText)[1]').getText()
-      logger.info(`before ${name}--number ${ny}`)
-      const r=/^[A-Za-z]{2}/
-      while(true){
-      if(r.test(name.trim())){
-      const dt=await axios.post('http://localhost:3000/bucket',{type:"insert",id:ny,name,image:pic},{validateStatus(status) {
-          return true
-      },timeout:30000})
-      if(dt.status==200){
-        break
-      }
-    }
-      logger.error(`Retrying During push data`)
-     }
-      logger.info(`${name}--number ${ny}`)
-        ny++
-        await elements.clearValue()
-        await elements.setValue(ny)
+        let ids = new Set()
+        logger.info(`Starting`)
+        let tr = 1
+        let ny
+        while (true) {
 
-      }
-      await sleep(2000)
-      
-      tr++
-    }
-    //    const el=await driver.$('//androidx.recyclerview.widget.RecyclerView[@resource-id="com.android.settings:id/recycler_view"]')
-    //    await driver.action("pointer",{})
-    //    const scn=await el.takeScreenshot()
-    //    fs.writeFileSync("./element.png",scn,"base64")
+            const elements = await driver.$('//XCUIElementTypeTextField[@name="Name, $Cashtag, Phone, Email"]')
 
-    
+            const text = await elements.getText()
+            if (text) {
+                ny = Number(text)
+                const el = await driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeButton)[1]')
+                const pic = await driver.takeElementScreenshot(el.elementId)
+                const name = await driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeStaticText)[1]').getText()
+                logger.info(`before ${name}--number ${ny}`)
+                const r = /^[A-Za-z]{2}/
+                while (true) {
+                    if (r.test(name.trim())) {
+                        const dt = await axios.post('http://localhost:3000/bucket', { type: "insert", id: ny, name, image: pic }, {
+                            validateStatus(status) {
+                                return true
+                            }, timeout: 30000
+                        })
+                        if (dt.status == 200) {
+                            break
+                        }
+                        logger.error(`getting error for push data ${ny}`)
+
+                    }
+                    break
+
+                }
+                logger.info(`${name}--number ${ny}`)
+                ny++
+                await elements.clearValue()
+                await elements.setValue(ny)
+
+            }
+            await sleep(2000)
+
+            tr++
+        }
+        //    const el=await driver.$('//androidx.recyclerview.widget.RecyclerView[@resource-id="com.android.settings:id/recycler_view"]')
+        //    await driver.action("pointer",{})
+        //    const scn=await el.takeScreenshot()
+        //    fs.writeFileSync("./element.png",scn,"base64")
+
+
 
     } finally {
         await driver.deleteSession();
---
+++ ios/srcy/index.js
@@ -0,0 +1,114 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const webdriverio_1 = require("webdriverio");
+const winston_1 = __importDefault(require("winston"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const papaparse_1 = __importDefault(require("papaparse"));
+const readline_1 = __importDefault(require("readline"));
+readline_1.default.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+let data = [];
+const customFormat = winston_1.default.format.printf(({ level, message, timestamp }) => {
+    return `${level.toUpperCase()} | ${timestamp} | ${message}`;
+});
+const logger = winston_1.default.createLogger({
+    level: 'info', // Default log level
+    format: winston_1.default.format.combine(winston_1.default.format.timestamp(), customFormat),
+    transports: [
+        //  new winston.transports.Console(), // Logs to the console
+        new winston_1.default.transports.File({ filename: path_1.default.join(__dirname + '/logs/app.log') }) // Logs to a file
+    ],
+});
+const axios_1 = __importDefault(require("axios"));
+const capabilities = {
+    "platformName": "iOS",
+    "appium:wdaBaseUrl": "http://10.0.0.27",
+    "appium:wdaLocalPort": 8100,
+    "appium:automationName": "XCUITest",
+    "appium:udid": "00008020-0015744C2608003A"
+};
+const wdOpts = {
+    hostname: 'localhost',
+    port: 4723,
+    logLevel: "error",
+    capabilities,
+    path: '/wd/hub',
+};
+const RandomMax = (max) => {
+    return Math.ceil(Math.random() * max);
+};
+function sleep(ms) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise(r => setTimeout(r, ms));
+    });
+}
+function runTest() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const driver = yield (0, webdriverio_1.remote)(wdOpts);
+        process.stdin.on("keypress", (str, key) => {
+            if (key.name === "j") {
+                console.log("\n'Q' key pressed. Exiting...");
+                const csv = papaparse_1.default.unparse(data);
+                fs_1.default.writeFileSync("./data.csv", csv, "utf8");
+                process.exit(0);
+            }
+        });
+        try {
+            let ids = new Set();
+            logger.info(`Starting`);
+            let tr = 1;
+            let ny;
+            while (true) {
+                let nhh;
+                const elements = yield driver.$('//XCUIElementTypeTextField[@name="Name, $Cashtag, Phone, Email"]');
+                const text = yield elements.getText();
+                if (text) {
+                    ny = Number(text);
+                    const el = yield driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeButton)[1]');
+                    const pic = yield driver.takeElementScreenshot(el.elementId);
+                    const name = yield driver.$('(//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther//XCUIElementTypeStaticText)[1]').getText();
+                    logger.info(`before ${name}--number ${ny}`);
+                    const r = /^[A-Za-z]{2}/;
+                    while (true) {
+                        if (r.test(name.trim())) {
+                            const dt = yield axios_1.default.post('http://localhost:3000/bucket', { type: "insert", id: ny, name, image: pic }, { validateStatus(status) {
+                                    return true;
+                                }, timeout: 30000 });
+                            if (dt.status == 200) {
+                                break;
+                            }
+                            logger.error(`getting error for push data ${ny}`);
+                        }
+                    }
+                    logger.info(`${name}--number ${ny}`);
+                    ny++;
+                    yield elements.clearValue();
+                    yield elements.setValue(ny);
+                }
+                yield sleep(2000);
+                tr++;
+            }
+            //    const el=await driver.$('//androidx.recyclerview.widget.RecyclerView[@resource-id="com.android.settings:id/recycler_view"]')
+            //    await driver.action("pointer",{})
+            //    const scn=await el.takeScreenshot()
+            //    fs.writeFileSync("./element.png",scn,"base64")
+        }
+        finally {
+            yield driver.deleteSession();
+        }
+    });
+}
+runTest();
