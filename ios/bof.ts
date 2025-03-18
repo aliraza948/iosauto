188a7631cbc706a1ae452c9c6ebea8392b5d3c9d
aliraza948 <aliraza948533@gmail.com>
2025-03-18 07:49:39 +0500

    fd

--- ios/bof.ts
+++ ios/bof.ts
@@ -114,7 +114,7 @@ async function Android() {
         //     const num=await driver.$(`((//androidx.appcompat.widget.LinearLayoutCompat[@resource-id="com.infonow.bofa:id/screen_zelle_pay_home_recipient_right_element_text"])//android.widget.TextView)[2]`).getText()
         //     const name=await driver.$(`((//androidx.appcompat.widget.LinearLayoutCompat[@resource-id="com.infonow.bofa:id/screen_zelle_pay_home_recipient_right_element_text"])//android.widget.TextView)[3]`).getText()
         
-        const d=await await driver.executeScript("return arguments[0].textContent;", [isText]);//isText.getText()     
+        const d= await driver.executeScript("return arguments[0].textContent;", [isText]);//isText.getText()     
         data.push({name:d})
              loggerf.info(`name:${d}--`)
         //  }
