17f828406df666f3b1415f27a6cd3feb7e85aa5a
aliraza948 <aliraza948533@gmail.com>
2025-03-18 07:18:34 +0500

    fd

--- ios/bof.ts
+++ ios/bof.ts
@@ -132,17 +132,17 @@ async function Android() {
     //     height: -400, // Move up by 400 pixels (adjust for scroll distance)
     //     duration: 1000 // Milliseconds (smoother swipe)
     //   });
-     await driver.execute('mobile: touchAndDrag', {
-        startX: 500, // Start position (adjust as needed)
-        startY: 500,
-        endX: 500,   // Keep same X to ensure vertical movement
-        endY: 300,   // End position (controls scroll amount)
-        duration: 1000 // Duration in ms (smooth scroll)
-      });
-    //   await driver.execute('mobile: scroll', {
-    //     direction: 'up',
-    //     percent: 0.5, // 1.0 = full screen scroll, 0.5 = half screen
+    //  await driver.execute('mobile: touchAndDrag', {
+    //     startX: 500, // Start position (adjust as needed)
+    //     startY: 500,
+    //     endX: 500,   // Keep same X to ensure vertical movement
+    //     endY: 300,   // End position (controls scroll amount)
+    //     duration: 1000 // Duration in ms (smooth scroll)
     //   });
+      await driver.execute('mobile: scroll', {
+        direction: 'up',
+        percent: 0.5, // 1.0 = full screen scroll, 0.5 = half screen
+      });
     //     await driver.performActions([
     //      {
     //        type: "pointer",
